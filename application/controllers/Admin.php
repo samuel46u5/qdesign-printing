fcaa450a6a238395262bc5ad782dde4dbdd14bca
samuel46u5 <49788892+samuel46u5@users.noreply.github.com>
2019-05-21 20:36:10 +0700

    ok

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -28,4 +28,46 @@ class Admin extends CI_Controller
         $this->load->view('admin/index', $data);
         $this->load->view('templates/footer');
     }
+
+    public function role()
+    {
+        $data['title'] = 'Admin';
+        $data['subtitle'] = 'Role';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $data['role'] = $this->db->get('adm_role')->result_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('admin/role', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function menu()
+    {
+        $data['title'] = 'Admin';
+        $data['subtitle'] = 'Menu';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        // echo 'selamat datang ' . $data['user']['nama'];
+
+        $data['menu'] = $this->db->get('adm_menu')->result_array();
+
+        // menganti error message , custome error message
+        $this->form_validation->set_message('required', 'Menu harus diisi');
+
+        $this->form_validation->set_rules('menu', 'Menu', 'required');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('menu/index', $data);
+            $this->load->view('templates/footer');
+        } else {
+            //tambah data menu
+            $this->db->insert('adm_menu', ['menu' => $this->input->post('menu')]);
+            $this->session->set_flashdata('message', 'Menu berhasil ditambah');
+            redirect('admin/menu');
+        }
+    }
 }
