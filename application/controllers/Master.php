5e7ea7a71e813c5e78207373f3c7b3226f66e115
samuel46u5 <49788892+samuel46u5@users.noreply.github.com>
2019-05-23 19:17:53 +0700

    ok

---
+++ application/controllers/Master.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Master extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        //ini cukup dipangil sekali Master_Model
+        $this->load->model('Master_Model');
+        is_logged_in();
+    }
+    public function kategori_mesin()
+    {
+        // ini harus diisi sesuai didatabase
+        $data['title'] = 'Master';
+        $data['subtitle'] = 'Kategori Mesin';
+        // agak menyala di menu
+
+
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('master/kategori_mesin', $data);
+        $this->load->view('templates/footer');
+    }
+}
